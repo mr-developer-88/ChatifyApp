86e7304fca10a1ba179ae85591e123e53314a0de
Mohammad Asim <strugler88@outlook.com>
2026-03-04 23:31:30 +0500

    Fixed Potentioal Issue, invalid/expires JWT bugs

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -10,10 +10,18 @@ export const protectRoute = async (req, res, next) => {
       return res.status(401).json({ message: "Unauthorized - No token provided" });
     }
 
-    const decoded = jwt.verify(token, ENV.JWT_SECRET);
-
-    if (!decoded || !decoded.userId) {
-      return res.status(401).json({ message: "Unauthorized - Invalid token" });
+    const { JWT_SECRET } = ENV;
+    if(!JWT_SECRET) {
+        console.error("JWT_SECRET is not configured!")
+        return res.status(500).json({ message:"Server misconfiguration"})
+    }
+    let decoded;
+    try {
+        decoded = jwt.verify(token, JWT_SECRET);
+    } catch (error) {
+        const message = error.name === "TokenExpiredError" ? "Unauthorized - Token expired" : "Unauthorized - Invalid token";
+        return res.status(401).json({ message: message
+         });
     }
 
     const user = await User.findById(decoded.userId).select("-password");
