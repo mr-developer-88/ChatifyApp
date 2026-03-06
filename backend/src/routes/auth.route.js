01d1fbcb21b623e7e11e90c3a5097829961fad2f
MrDeveloper <117296386+mr-developer-88@users.noreply.github.com>
2026-03-06 15:07:39 +0500

    Update auth.route.js

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,12 +1,8 @@
 import express from 'express';
 import { signup, login, logout, updateProfile } from '../controllers/auth.controller.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
-import { arcjetProtection } from '../middleware/arcjet.middleware.js';
-
 
 const router = express.Router();
-
-router.use(arcjetProtection);
 
 router.post('/signup', signup);
 router.post('/login', login);
