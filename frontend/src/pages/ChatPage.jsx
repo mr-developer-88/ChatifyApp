94187e9f1b75537418cb16295ad658f354031283
Mohammad Asim <strugler88@outlook.com>
2026-03-09 20:47:25 +0500

    side bar completed normally

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -4,9 +4,9 @@ import BorderAnimatedContainer from "../components/BorderAnimatedContainer";
 import ProfileHeader from "../components/ProfileHeader";
 import ActiveTabSwitch from "../components/ActiveTabSwitch";
 import ChatsList from "../components/ChatsList";
-import ContactList from "../components/ContactList";
 import ChatContainer from "../components/ChatContainer";
 import NoConversationPlaceholder from "../components/NoConversationPlaceholder";
+import ContactList from "../components/ContactList";
 
 function ChatPage() {
   const { activeTab, selectedUser } = useChatStore();
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios"
 import toast from "react-hot-toast";
+import axios from "axios";
 
 export const useAuthStore = create((set) => ({
   authUser: null,
@@ -56,6 +57,17 @@ export const useAuthStore = create((set) => ({
     } catch(error) {
       toast.error("Error logging out!")
       console.log("Logout Error: ", error)
+    }
+  },
+
+  updateProfile: async (data) => {
+    try {
+      const res = await axiosInstance.put("/auth/update-profile", data);
+      set({authUser: res.data})
+      toast.success("Profile Update successfully!")
+    } catch (error) {
+      console.log("Error in update profile:", error)
+      toast.error(error.response.data.message)
     }
   }
 }));
